81b9ebc14f085c79a40a8547cd0a869c766459ca
sado98 <sado9898kz@gmail.com>
2020-03-01 21:27:11 +0600

    How work onSaveInstanceState

--- app/src/main/java/ru/android/start/save/MainActivity.kt
+++ app/src/main/java/ru/android/start/save/MainActivity.kt
@@ -1,12 +1,32 @@
 package ru.android.start.save
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = editText.text
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        outState.run {
+            putString("KEY", editText.text.toString())
+        }
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        textView.text = savedInstanceState.getString("KEY")
     }
 }
